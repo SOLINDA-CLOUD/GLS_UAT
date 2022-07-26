37944f840346f6186fb4a5d9fd4520b3bf0846f6
inicoding <irfannurrahmat@outlook.com>
2022-07-26 09:27:41 +0700

    fix register payment

--- solinda_accounting/models/account_payment.py
+++ solinda_accounting/models/account_payment.py
@@ -5,3 +5,8 @@ class AccountPayment(models.TransientModel):
 
     other_costs = fields.Monetary(string='Additional Other Costs')
     other_account = fields.Many2one('account.account', string='Additional Account', ondelete='cascade')
+
+    @api.onchange('other_account')
+    def _onchange_other_account(self):
+        domain = {'other_account': [('user_type_id', '=', 'expenses')]}
+        return {'domain': domain}
